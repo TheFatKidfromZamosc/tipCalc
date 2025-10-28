a8f66884538745099b6d3a143efd38deb21f4a34
TheFatKidFromZamosc <witiuk61@gmail.com>
2025-10-28 11:13:43 +0100

    dsafasfd

---
+++ calc/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "calc")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "calc.Pages")]
---
+++ calc/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace calc
+{
+    public partial class MainPage : ContentPage
+    {
+        double kwota = 0;
+        int oprocentowanie = 0;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void OnButtonClicked(object? sender, EventArgs e)
+        {
+            kwota = double.Parse(kwotaRachunku.Text);
+            double wynik = kwota + (kwota * oprocentowanie / 100);
+            napiwekLabel.Text = "Napiwek : " + (kwota * oprocentowanie / 100)+ " zł";
+            wynikLabel.Text = "Do zapłaty : " + wynik + " zł";
+        }
+
+        private void OnSliderValueChanged(object? sender, EventArgs e)
+        {
+          Slider slider = (Slider)sender;
+            oprocentowanie = (int)slider.Value;
+            
+        }
+        private void OnResetClicked(object? sender, EventArgs e)
+        {
+            kwotaRachunku.Text = "";
+            napiwekLabel.Text = "";
+            wynikLabel.Text = "";
+            procentNapiwku.Value = 15;
+        }
+    }
+}
